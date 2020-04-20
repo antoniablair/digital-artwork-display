52ff8a52a399393deaeb4a8571fb783cc88b3e6f
Antonia Blair <antoniablair@gmail.com>
2020-04-20 16:11:39 -0400

    Remove clutter from nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,44 +1,24 @@
 import React from "react";
-
+import Query from "../Query";
 import { Link } from "react-router-dom";
 
+import SLIDESHOWS_QUERY from "../../queries/slideshows/slideshows";
 
 const Nav = () => {
   return (
     <div>
-      return (
-        <div>
-          <nav className="uk-navbar-container" data-uk-navbar>
-            <div className="uk-navbar-left">
-              <ul className="uk-navbar-nav">
-                <li>
-                  <Link to="/">Slideshows</Link>
-                </li>
-                <li>
-                  <Link to="/instructions">Instructions</Link>
-                </li>
-              </ul>
-            </div>
-
-            {/* <div className="uk-navbar-right">
-              <ul className="uk-navbar-nav">
-                {slideshows.map((slideshow, i) => {
-                  return (
-                    <li key={slideshow.id}>
-                      <Link
-                        to={`/slideshow/${slideshow.id}`}
-                        className="uk-link-reset"
-                      >
-                        {slideshow.name}
-                      </Link>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div> */}
-          </nav>
+      <nav className="uk-navbar-container" data-uk-navbar>
+        <div className="uk-navbar-left">
+          <ul className="uk-navbar-nav">
+            <li>
+              <Link to="/">Slideshows</Link>
+            </li>
+            <li>
+              <Link to="/instructions">Instructions</Link>
+            </li>
+          </ul>
         </div>
-      );
+      </nav>
     </div>
   );
 };
