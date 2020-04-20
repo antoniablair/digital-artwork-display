bff92be6afef1829a6c2da87e3e257635d4c9c1b
Antonia Blair <antoniablair@gmail.com>
2020-04-20 16:14:01 -0400

    Add instructions

--- src/components/Instructions/index.js
+++ src/components/Instructions/index.js
@@ -4,8 +4,15 @@ const Instructions = () => (
   <div>
     <div>
       <h1>Instructions</h1>
-      <p>To run the art in full-screen mode (similar to a digital photograph), open 
-         Google Chrome in kiosk mode and select play.
+      <p>
+        This is a frontend application for displaying GIFs or other images in a digital photo album. 
+      </p>
+      <p>
+        Suggested use is to run Google Chrome in "kiosk mode" on an iPad or other tablet, 
+        and then click on one of the slideshows to launch the slideshow player. The slideshow elements will loop infinitely without displaying the browser. You can then mount the iPad / tablet in a frame on the wall if desired. 
+      </p>
+      <p>
+        To exit Kiosk mode on iPad, triple-click the iPad home button, enter your Guided Access passcode and tap End in the top left.
       </p>
     </div>
   </div>
